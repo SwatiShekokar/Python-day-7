e50bbe3306f554c84e1235e45cf73bbf1600d284
SwatiShekokar <85446361+SwatiShekokar@users.noreply.github.com>
2022-02-08 00:11:09 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,17 @@
+
+import mysql.connector as sql
+
+# Connecting the local mysql database
+database = sql.connect(host = 'localhost', user = 'root', passwd = 'ferrari458', database = 'schooldata')
+
+cur = database.cursor()
+
+
+cur.execute("update student set Eng_mark = 80 where Roll_no = 122")
+cur.execute("select * from student")
+
+for i in cur:
+    print(i)
+
+cur.close()
+database.commit()
